f89c29117c3c51ce297955cb0b040d40e75c5c2e
Túlio de Freitas Castro <tulioneedforspeed@gmail.com>
2019-04-11 19:16:11 -0300

    Envoltoria documentada

--- Algoritmo-Geometrico/Envoltoria convexa.py
+++ Algoritmo-Geometrico/Envoltoria convexa.py
@@ -4,9 +4,13 @@ Created on Sun Mar 10 15:28:48 2019
 
 @author: Túlio
 """
-from math import atan2,sqrt
+#Algoritmo da Envoltória convexa utilizando o modelo de Graham que assintoticamente roda em O(nlogn),segundo os ensinamentos do livro de Udi Manber: A introduction to algorithms
 
-def particao(v,esq,direi):
+
+from math import atan2,sqrt #Importa-se da biblioteca Math, sqrt que é raiz quadrada
+                            #e o arcotangente para calcular o angulo formado por 3 pontos no plano tendo como base o eixo X.
+
+def particao(v,esq,direi): #A receurisividade do qsort chama a particao toda vez ate esta tudo ordenado
     pivo=dist_seg(v[direi])
     pivo2=coef(v[direi])
     i=esq-1
@@ -18,8 +22,8 @@ def particao(v,esq,direi):
     return i+1
     
     
-def qsort(v,esq,direi):
-    while esq<direi:
+def qsort(v,esq,direi): # QuickSort adaptado para encontrar em ordem crescente os angulos do arcotangente, em caso de ser igual, leva 
+    while esq<direi:    # em consideracao a distancia de segmentos
         pi=particao(v,esq,direi)
         if pi - esq < direi - pi:
             qsort(v,esq,pi - 1)
@@ -29,7 +33,7 @@ def qsort(v,esq,direi):
             direi = pi - 1
     return v
     
-def coef(x,y=None):
+def coef(x,y=None): # Esta funcao determina o angulo entre os pontos que eh a principal pivo do quicksort adaptado
   if y==None:
     y=ancora
   ys=(x[1]-y[1])
@@ -37,20 +41,20 @@ def coef(x,y=None):
   angulo=atan2(ys,xs)
   return angulo
 
-def orientacao(p,q,r):
-
+def orientacao(p,q,r): #funcao para encontrar a orientacao dos segmentos de retas formadas entre os pontos, dando valor negativo eh anti horario
+                       # dando 0 eh colinear, positivo horario.
   val = (q[0] - p[0]) * (r[1] - p[1]) \
       - (q[1] - p[1]) * (r[0] - p[0])
   return val
 
-def dist_seg(a,b=None):
-  if b==None:
+def dist_seg(a,b=None): # Essa distancia de segmento irá servir como segundo argumento na adaptação do quicksort para a construção da
+  if b==None:           #envoltoria
     b=ancora
   bs=(a[1]-b[1])
   a_s=(a[0]-b[0])
   return bs**2 + a_s**2
 
-def tupla_min(arr):
+def tupla_min(arr): #Funcao utilizada para encontrar o ponto mais extremo, nesse caso maior y e menor x.
   ymin,xmax=arr[0][1],arr[0][0]
   for i in range(1,len(arr)):
     y=arr[i][1]
@@ -60,23 +64,24 @@ def tupla_min(arr):
   return xmax,ymin
 
 def convexa(v):
-  global ancora 
-  ancora = tupla_min(v)
-  ordenado=qsort(v,0,len(v)-1)
-  del ordenado[ordenado.index(ancora)]
-  envo=[ancora,ordenado[0]]
-  m=len(envo)
+  global ancora #Cria-se uma var global para utilizacao em algumas funcoes. Ex: dit_seg,coef
+  ancora = tupla_min(v) #Essa variavel vira o ponto mais extremo do plano
+  ordenado=qsort(v,0,len(v)-1) #cria um vetor ordenado segundo a distancia e o angulo formado entre os pontos
+  del ordenado[ordenado.index(ancora)] #retira-se a var ancora
+  envo=[ancora,ordenado[0]] #cria um vetor com dois valores para a construcao do menor poligono possivel, o triangulo.
+  m=len(envo) #salva o tamanho desse poligono
   for s in range(1,len(ordenado)):
     
-    while orientacao(envo[m-2],envo[m-1],ordenado[s])<=0:
-      del envo[m-1]
-      m=len(envo)
+    while orientacao(envo[m-2],envo[m-1],ordenado[s])<=0: #percorre as listas envo e ordenado 
+      del envo[m-1]                                       #procurando as envoltorias q abrange todos os pontos
+      m=len(envo) #atualiza o tamanho
       #if len(hull)<2: break
-    envo.append(ordenado[s])
-    m=len(envo)
-  return envo
-  
-def dist_seg2(x,y):
+    envo.append(ordenado[s]) #adiciona os valores ja computados
+    m=len(envo) #novamente atualiza  os tamanhos
+  return envo #retorna os pontos que formam a envoltoria convexa
+
+
+def dist_seg2(x,y): #Funcao para calcular a distancia entre dois pontos no plano
   ys=(x[1]-y[1])
   xs=(x[0]-y[0])
   return sqrt(ys**2+xs**2)
